f585dadf4016d5092ad55774fe376f88cc6fd06f
MarcoSac1 <marcosaccone8@gmail.com>
2024-07-01 16:53:14 +0200

    created 20 todo array

--- server.php
+++ server.php
@@ -1,31 +1,37 @@
 <?php 
 
-$toDo =[
-    [
-        "nome" => "Prenotazione medica",
-        "luogo"=> "Ospedale",
-        "scadenza"=> "2024-07-15"
-    ],
-    [
-        "nome" => "Ritiro pacchetto",
-        "luogo" => "Posta",
-        "scadenza" => "2024-07-10"
-    ],
-    [
-        "nome" => "Prenotazione viaggio",
-        "luogo" => "Aeroporto",
-        "scadenza" => "2024-08-01"
-    ],
-    [
-        "nome" => "Pagamento fattura",
-        "luogo" => "Banca",
-        "scadenza" => "2024-07-20"
-    ]
-];    
+// $toDo =[
+//     [
+//         "nome" => "Prenotazione medica",
+//         "luogo"=> "Ospedale",
+//         "scadenza"=> "2024-07-15"
+//     ],
+//     [
+//         "nome" => "Ritiro pacchetto",
+//         "luogo" => "Posta",
+//         "scadenza" => "2024-07-10"
+//     ],
+//     [
+//         "nome" => "Prenotazione viaggio",
+//         "luogo" => "Aeroporto",
+//         "scadenza" => "2024-08-01"
+//     ],
+//     [
+//         "nome" => "Pagamento fattura",
+//         "luogo" => "Banca",
+//         "scadenza" => "2024-07-20"
+//     ]
+// ];    
 
-// var_dump($toDo);
+// // var_dump($toDo);
 
 header('content-type:application/json');
 
-echo json_encode($toDo);
+// echo json_encode($toDo);
+
+$rawData = file_get_contents('./db/todo.json');
+$data = json_decode($rawData, true );
+
+// var_dump($rawData,  $data);
+echo json_encode($data);
 ?>
