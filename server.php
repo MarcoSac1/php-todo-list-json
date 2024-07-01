8e175ee811c79195775f85c6efbb957b553d5794
MarcoSac1 <marcosaccone8@gmail.com>
2024-07-01 16:26:40 +0200

    created json and printed

--- server.php
+++ server.php
@@ -1,6 +1,31 @@
 <?php 
 
+$toDo =[
+    [
+        "nome" => "Prenotazione medica",
+        "luogo"=> "Ospedale",
+        "scadenza"=> "2024-07-15"
+    ],
+    [
+        "nome" => "Ritiro pacchetto",
+        "luogo" => "Posta",
+        "scadenza" => "2024-07-10"
+    ],
+    [
+        "nome" => "Prenotazione viaggio",
+        "luogo" => "Aeroporto",
+        "scadenza" => "2024-08-01"
+    ],
+    [
+        "nome" => "Pagamento fattura",
+        "luogo" => "Banca",
+        "scadenza" => "2024-07-20"
+    ]
+];    
 
+// var_dump($toDo);
 
 header('content-type:application/json');
+
+echo json_encode($toDo);
 ?>
